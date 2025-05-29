15395ef59a8ad7ddb392f9904fddd868a67a878d
Seeloewen <Louis.Herbert2006@gmail.com>
2025-05-30 00:35:45 +0200

    Implement click events for blocks/floors

--- src/main/java/de/sealcore/game/floors/Floor.java
+++ src/main/java/de/sealcore/game/floors/Floor.java
@@ -10,4 +10,16 @@ public abstract class Floor
         this.id = id;
         this.name = name;
     }
+
+    public int onLeftClick()
+    {
+        //Returns -1 if not implemented. Should return 0 if implemented to avoid unwanted behaviour!
+        return -1;
+    }
+
+    public int onRightClick()
+    {
+        //Returns -1 if not implemented. Should return 0 if implemented to avoid unwanted behaviour!
+        return -1;
+    }
 }
