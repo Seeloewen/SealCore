3071c1c75be9c08ebb520d5a9ce9c944b432e53e
Seeloewen <Louis.Herbert2006@gmail.com>
2025-06-17 03:05:19 +0200

    Update TypeParser

--- src/main/java/de/sealcore/util/TypeParser.java
+++ src/main/java/de/sealcore/util/TypeParser.java
@@ -2,11 +2,11 @@ package de.sealcore.util;
 
 public class TypeParser
 {
-    public boolean IsInt(String s)
+    public static boolean isInt(String s)
     {
         try
         {
-            Integer.parseInt(s); //Try to parseInput, if no exception is thrown it will return true
+            Integer.parseInt(s); //Try to parse Input, if no exception is thrown it will return true
             return true;
         }
         catch(Exception ex)
@@ -15,13 +15,26 @@ public class TypeParser
         }
     }
 
-    public boolean IsBool(String s)
+    public static boolean isDouble(String s)
+    {
+        try
+        {
+            Double.parseDouble(s); //Try to parse Input, if no exception is thrown it will return true
+            return true;
+        }
+        catch(Exception ex)
+        {
+            return false; //If exception thrown, we can assume it's not an int
+        }
+    }
+
+    public static boolean isBool(String s)
     {
         //Checks if it's either true or false
         return s.equalsIgnoreCase("true") || s.equalsIgnoreCase("false");
     }
 
-    public int GetInt(String s)
+    public static int getInt(String s)
     {
         //Returns 0 by default. To avoid mistaking this 0 as
         //the actual result of the parseInput, it's recommended to check first if it's parseable
@@ -35,7 +48,21 @@ public class TypeParser
         }
     }
 
-    public boolean GetBool(String s)
+    public static double getDouble(String s)
+    {
+        //Returns 0 by default. To avoid mistaking this 0 as
+        //the actual result of the parseInput, it's recommended to check first if it's parseable
+        try
+        {
+            return Double.parseDouble(s);
+        }
+        catch(Exception ex)
+        {
+            return 0;
+        }
+    }
+
+    public static boolean getBool(String s)
     {
         return Boolean.getBoolean(s);
     }
